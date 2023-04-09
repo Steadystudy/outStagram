7d0c7eb8dc997d79716bd005280bb65dc01613fc
Steadystudy <sgmin206@naver.com>
2023-04-09 16:28:07 +0900

    Feat: Sign in, out시 Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,6 +4,7 @@ import NavIcon from './ui/icons/NavIcon';
 import { usePathname } from 'next/navigation';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import StyledButton from './ui/StyledButton';
+import Avatar from './Avatar';
 
 const navList = [
   { icon: NavIcon('home'), iconFill: NavIcon('homeFill'), link: '/' },
@@ -13,35 +14,45 @@ const navList = [
 export default function Header() {
   const pathname = usePathname();
   const { data: session } = useSession();
+  const user = session?.user;
   return (
     <header className="sticky top-0 bg-white z-10 border-b border-gray-hot px-6">
       <div className="flex justify-between items-center">
         <Link href="/">
           <h1 className="text-pink-light text-2xl font-bold">Outstagram</h1>
         </Link>
-        <nav className="flex items-center">
+        <nav>
           <ul className="flex gap-4 items-center p-4">
             {navList.map(({ icon, iconFill, link }) => (
               <li key={link}>
                 <Link href={link}>{pathname === link ? iconFill : icon}</Link>
               </li>
             ))}
+            {user && (
+              <li>
+                <Link href={`/user/${user.username}`}>
+                  <Avatar image={user.image} />
+                </Link>
+              </li>
+            )}
+            <li>
+              {session ? (
+                <StyledButton
+                  text="Sign Out"
+                  onClick={() => {
+                    signOut();
+                  }}
+                />
+              ) : (
+                <StyledButton
+                  text="Sign in"
+                  onClick={() => {
+                    signIn();
+                  }}
+                />
+              )}
+            </li>
           </ul>
-          {session ? (
-            <StyledButton
-              text="Sign Out"
-              onClick={() => {
-                signOut();
-              }}
-            />
-          ) : (
-            <StyledButton
-              text="Sign in"
-              onClick={() => {
-                signIn();
-              }}
-            />
-          )}
         </nav>
       </div>
     </header>
--- src/app/components/ui/StyledButton.tsx
+++ src/app/components/ui/StyledButton.tsx
@@ -1,13 +1,20 @@
 type Props = {
   text: string;
   onClick: () => void;
+  size?: 'sm' | 'xl';
 };
 
-export default function StyledButton({ text, onClick }: Props) {
+export default function StyledButton({ text, onClick, size = 'sm' }: Props) {
   return (
-    <div className="rounded-md bg-gradient-to-r from-pink-hot via-pink-light to-yellow-light p-[0.2rem]">
+    <div
+      className={`rounded-md bg-gradient-to-r from-pink-hot via-pink-light to-yellow-light
+      ${size === 'sm' ? `p-[0.3rem]` : `p-[0.2rem]`}
+    `}
+    >
       <button
-        className="bg-white rounded-sm p-[0.1rem] hover:opacity-75 transition-opacity"
+        className={`bg-white rounded-sm hover:opacity-75 transition-opacity
+          ${size === 'sm' ? `p-[0.1rem] text-base` : `p-2 text-2xl`}
+        `}
         onClick={onClick}
       >
         {text}
