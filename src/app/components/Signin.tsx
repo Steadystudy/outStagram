d1fed31a190f8c11e9af5dd0209f0e38d43e3296
Steadystudy <sgmin206@naver.com>
2023-04-09 16:25:47 +0900

    Feat: signin client component

---
+++ src/app/components/Signin.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { ClientSafeProvider, signIn } from 'next-auth/react';
+import StyledButton from './ui/StyledButton';
+
+type Props = {
+  providers: Record<string, ClientSafeProvider>;
+  callbackUrl: string;
+};
+
+export default function Signin({ providers, callbackUrl }: Props) {
+  return (
+    <>
+      {Object.values(providers).map(({ id, name }) => (
+        <div key={name}>
+          <StyledButton
+            size="xl"
+            onClick={() => signIn(id, { callbackUrl })}
+            text={`Sign in with ${name}`}
+          />
+        </div>
+      ))}
+    </>
+  );
+}
