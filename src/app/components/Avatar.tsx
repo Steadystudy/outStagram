1e9a27f2995a8fb4539364e616868f150fdcd211
Steadystudy <sgmin206@naver.com>
2023-04-10 23:32:06 +0900

    Style: Avatar

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -1,12 +1,20 @@
-type Props = { image?: string | null };
+type Props = {
+  image?: string | null;
+  size?: 'sm' | 'md';
+  border?: true | false;
+};
 
-export default function Avatar({ image }: Props) {
+export default function Avatar({ image, size = 'sm', border = true }: Props) {
   return (
-    <div className="rounded-full w-9 h-9">
+    <div
+      className={`rounded-full 
+      ${border ? 'bg-gradient-to-tr from-yellow-light via-pink-light to-pink-hot' : ''}
+      ${size === 'sm' ? `p-[0.15rem] w-10 h-10` : `p-[0.2rem] w-14 h-14`}`}
+    >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src={image ?? undefined}
-        className="rounded-full"
+        className="rounded-full border-2 border-white"
         alt={'user profile'}
         referrerPolicy="no-referrer"
       />
