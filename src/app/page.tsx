1dddee0514dcb7615bb773da602fa4164518484c
Steadystudy <sgmin206@naver.com>
2023-04-10 23:32:38 +0900

    Feat: Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <main className="bg-black">Home</main>;
+import { getServerSession } from 'next-auth';
+import FollowingBar from './components/FollowingBar';
+import PostList from './components/PostList';
+import Sidebar from './components/Sidebar';
+import { redirect } from 'next/navigation';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    redirect('/auth/signin');
+  }
+
+  return (
+    <main>
+      <FollowingBar />
+      <PostList />
+      <Sidebar user={user} />
+    </main>
+  );
 }
