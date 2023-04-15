03391db2bc8ea2e3a01c20f345d17ee8d040de69
Steadystudy <sgmin206@naver.com>
2023-04-15 18:51:38 +0900

    Feat: Post Card

---
+++ src/app/components/PostCard.tsx
@@ -0,0 +1,36 @@
+import { SimplePost } from '@/model/posts';
+import Avatar from './Avatar';
+import PostIcon from './ui/icons/PostIcon';
+import Image from 'next/image';
+import { parseDate } from '@/util/date';
+
+export default function PostCard({ post }: { post: SimplePost }) {
+  const { username, userImage, createdAt, image, text, likes } = post;
+
+  return (
+    <article>
+      <div>
+        <Avatar image={userImage} />
+        <span>{username}</span>
+      </div>
+      <Image src={image} alt={`photo by ${username}`} width={500} height={500} />
+      <div>
+        {PostIcon('heart')}
+        {PostIcon('bookmark')}
+      </div>
+      <div>
+        <p>{`${likes?.length ?? 0} ${likes?.length > 1 ? 'likes' : 'like'}`}</p>
+        <p>
+          <span>{username}</span>
+          {text}
+        </p>
+        <p>{parseDate(createdAt)}</p>
+      </div>
+      <form>
+        {PostIcon('smile')}
+        <input type="text" placeholder="Comment" />
+        <button>POST</button>
+      </form>
+    </article>
+  );
+}
