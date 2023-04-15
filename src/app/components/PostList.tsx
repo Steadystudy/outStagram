16dc57e15901b0c891a59b53bc8d5050691f1920
Steadystudy <sgmin206@naver.com>
2023-04-15 18:52:26 +0900

    Feat: PostCard를 나열하는 List

--- src/app/components/PostList.tsx
+++ src/app/components/PostList.tsx
@@ -1,3 +1,22 @@
+'use client';
+import { SimplePost } from '@/model/posts';
+import useSWR from 'swr';
+import PostCard from './PostCard';
+
 export default function PostList() {
-  return <div>PostList</div>;
+  const { data: posts, isLoading } = useSWR<SimplePost[]>('/api/posts');
+
+  return (
+    <section>
+      {isLoading && <div></div>}
+      <ul>
+        {posts &&
+          posts.map((post) => (
+            <li key={post.id}>
+              <PostCard post={post}></PostCard>
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
 }
