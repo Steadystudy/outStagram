643271e95b670e45621a38447f96ed873801e43d
Steadystudy <sgmin206@naver.com>
2023-04-28 18:10:53 +0900

    Refactor: session user 필요한 api에 withSessionUser 함수로 refactor

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,16 +1,10 @@
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { getUserByUsername } from '@/service/user';
-import { getServerSession } from 'next-auth';
+import { withSessionUser } from '@/util/session';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-
-  if (!user) {
-    return new Response('Authentication Error', { status: 401 });
-  }
-
-  return getUserByUsername(user.username) //
-    .then((res) => NextResponse.json(res));
+  return withSessionUser(async (user) => {
+    return getUserByUsername(user.username) //
+      .then((res) => NextResponse.json(res));
+  });
 }
